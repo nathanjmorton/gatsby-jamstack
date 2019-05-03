52e7f3030099533405ac949a645c2fa6613fe825
Nathan Morton <nathan.j.morton@gmail.com>
2019-05-03 02:12:31 -0700

    this for netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => {
 			<Head title="Home" />
 			<h1>Hello</h1>
 			<h2>I'm Nathan, a full-stack developer living in the Silicon Valley CA Bay Area</h2>
+			<p>Change for netlify.</p>
 		</Layout>
 	);
 };
